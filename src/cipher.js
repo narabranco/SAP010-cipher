599ab1f95fb3baef30a9183506a08378c7a9488b
NaraBranco <alicebradao@gmail.com>
2023-05-02 19:09:19 -0300

    push final

--- src/cipher.js
+++ src/cipher.js
@@ -1,122 +1,52 @@
 const cipher = {
-  encode: function (string, offset) {
-    let newText = "";
-    let num;
-    for (let i = 0; i < string.length; i++) {
-      num = string.charCodeAt([i]);
-      if (num >= 65 && num <= 90) {
-        num = ((num - 65 + offset) % 26) + 65;
-        newText = newText.concat(String.fromCharCode(num));
+  encode: function (offset, string) {
+    if (typeof offset !== 'number' || typeof string !== 'string') {
+      throw new TypeError('Os argumentos devem ser uma string e um número');
+    } else {
+      let newText = "";
+      let num;
+
+      for (let i = 0; i < string.length; i++) {
+        num = string.charCodeAt([i]);
+        if (num >= 65 && num <= 90) {
+          num = ((num - 65 + offset) % 26) + 65;
+          newText = newText.concat(String.fromCharCode(num));
+        }
+        else if (num >= 97 && num <= 122) {
+          num = ((num - 97 + offset) % 26) + 97;
+          newText = newText.concat(String.fromCharCode(num));
+        }
+        else {
+          newText = newText.concat(String.fromCharCode(num));
+        }
       }
-      else if (num >= 97 && num <= 122) {
-        num = ((num - 97 + offset) % 26) + 97;
-        newText = newText.concat(String.fromCharCode(num));
-      }
-      else {
-        newText = newText.concat(String.fromCharCode(num));
-      }
+      return newText;
     }
-    return newText;
   },
-  decode: function (string, offset) {
-    let newText = "";
-    let num;
-    for (let i = 0; i < string.length; i++) {
-      num = string.charCodeAt([i]);
-      if (num >= 65 && num <= 90) {
-        num = ((num - 90 - offset) % 26) + 90;
-        newText = newText.concat(String.fromCharCode(num));
+  decode: function (offset, string) {
+    if (typeof offset !== 'number' || typeof string !== 'string') {
+      throw new TypeError('Os argumentos devem ser uma string e um número');
+    } else {
+      let newText = "";
+      let num;
+      for (let i = 0; i < string.length; i++) {
+        num = string.charCodeAt([i]);
+        if (num >= 65 && num <= 90) {
+          num = ((num - 90 - offset) % 26) + 90;
+          newText = newText.concat(String.fromCharCode(num));
+        }
+        else if (num >= 97 && num <= 122) {
+          num = ((num - 122 - offset) % 26) + 122;
+          newText = newText.concat(String.fromCharCode(num));
+        }
+        else {
+          newText = newText.concat(String.fromCharCode(num));
+        }
       }
-      else if (num >= 97 && num <= 122) {
-        num = ((num - 122 - offset) % 26) + 122;
-        newText = newText.concat(String.fromCharCode(num));
-      }
-      else {
-        newText = newText.concat(String.fromCharCode(num));
-      }
+      return newText;
     }
-    return newText;
   }
 };
 
+
 export default cipher;
-
-
-//alert("test");
-
-// document.getElementById("cifrar").addEventListener("click", encode);
-// document.getElementById("decifrar").addEventListener("click", decode);
-// document.getElementById("limpar").addEventListener("click", limpar);
-
-
-// function encode() {
-//   const string = document.getElementById("texto").value;
-//   const offset = parseInt(document.getElementById("offset").value);
-//   const result = cifrar(string, offset);
-
-//   document.getElementById("result").innerHTML =
-//     ("<h3 id='titulo2'>Mensagem cifrada:</h3>" +
-//       "<textarea rows=5 cols=70>" + result + "</textarea><br>");
-// }
-
-
-
-//procurando como ligar a função do encode e decode com a logica do cifrar e decifrar
-//https://pt.stackoverflow.com/questions/26347/chamar-função-js-em-outro-arquivo-dependência-entre-scripts-javascript
-// window.cifrar = function (string, offset){
-//   let newText = "";
-//   let num;
-//   for (let i = 0; i < string.length; i++) {
-//     num = string.charCodeAt([i]);
-//     if (num >= 65 && num <= 90) {
-//       num = ((num - 65 + offset) % 26) + 65;
-//       newText = newText.concat(String.fromCharCode(num));
-//     }
-//     else if (num >= 97 && num <= 122) {
-//       num = ((num - 97 + offset) % 26) + 97;
-//       newText = newText.concat(String.fromCharCode(num));
-//     }
-//     else {
-//       newText = newText.concat(String.fromCharCode(num));
-//     }
-//   }
-//   return newText;
-// }
-
-//#####################//
-
-//procurando como ligar a função do encode e decode com a logica do cifrar e decifrar
-//https://pt.stackoverflow.com/questions/26347/chamar-função-js-em-outro-arquivo-dependência-entre-scripts-javascript
-// window.decifrar = function (string, offset){
-//   let newText = "";
-//   let num;
-//   for (let i = 0; i < string.length; i++) {
-//     num = string.charCodeAt([i]);
-//     if (num >= 65 && num <= 90) {
-//       num = ((num - 90 - offset) % 26) + 90;
-//       newText = newText.concat(String.fromCharCode(num));
-//     }
-//     else if (num >= 97 && num <= 122) {
-//       num = ((num - 122 - offset) % 26) + 122;
-//       newText = newText.concat(String.fromCharCode(num));
-//     }
-//     else {
-//       newText = newText.concat(String.fromCharCode(num));
-//     }
-//   }
-//   return newText;
-// }
-
-// function decode() {
-//   const string = document.getElementById("texto").value;
-//   const offset = parseInt(document.getElementById("offset").value);
-//   const result = decifrar(string, offset);
-
-//   document.getElementById("result").innerHTML =
-//     ("<h3 id='titulo2'>Mensagem decifrada:</h3>" +
-//       "<textarea rows=5 cols=70>" + result + "</textarea><br>");
-// }
-
-// function limpar() {
-//   location.reload();
-// }
--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import cipher from './cipher.js';
 
-//codigo duplicado, neste arquivo não funciona
-document.getElementById("cifrar").addEventListener("click", encode);
-document.getElementById("decifrar").addEventListener("click", decode);
-document.getElementById("limpar").addEventListener("click", limpar);
+
+const btnEncode = document.getElementById("cifrar");
+btnEncode.addEventListener("click", encode);
+
+const btnDencode = document.getElementById("decifrar");
+btnDencode.addEventListener("click", decode);
+
+const btnLimpar = document.getElementById("limpar");
+btnLimpar.addEventListener("click", limpar);
 
 function encode() {
-  const string = document.getElementById("root").value;
+  const string = document.getElementById("textEncode").value;
   const offset = parseInt(document.getElementById("offset").value);
-  const result = cipher.encode(string, offset);
+  const result = cipher.encode(offset, string);
 
   document.getElementById("result").innerHTML =
     ("<h3 id='titulo2'>Mensagem cifrada:</h3>" +
@@ -16,10 +21,11 @@ function encode() {
 }
 
 function decode () {
-  const string = document.getElementById("root").value;
+  const string = document.getElementById("textEncode").value;
   const offset = parseInt(document.getElementById("offset").value);
-  const result = cipher.decode(string, offset);
+  const result = cipher.decode(offset, string);
 
+  
   document.getElementById("result").innerHTML =
         ("<h3 id='titulo2'>Mensagem decifrada:</h3>" +
             "<textarea rows=5 cols=70>" + result + "</textarea><br>");
@@ -27,5 +33,6 @@ function decode () {
 
 function limpar() {
   location.reload();
+  
 }
 
