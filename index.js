f9d9a62296387d1a77054c0eb5e4cd643cff6ce6
NaraBranco <alicebradao@gmail.com>
2023-05-02 19:13:16 -0300

    Updates

--- index.js
+++ index.js
@@ -1,6 +1,38 @@
 import cipher from './cipher.js';
 
 
-console.log(cipher);
+const btnEncode = document.getElementById("cifrar");
+btnEncode.addEventListener("click", encode);
 
+const btnDencode = document.getElementById("decifrar");
+btnDencode.addEventListener("click", decode);
+
+const btnLimpar = document.getElementById("limpar");
+btnLimpar.addEventListener("click", limpar);
+
+function encode() {
+  const string = document.getElementById("textEncode").value;
+  const offset = parseInt(document.getElementById("offset").value);
+  const result = cipher.encode(offset, string);
+
+  document.getElementById("result").innerHTML =
+    ("<h3 id='titulo2'>Mensagem cifrada:</h3>" +
+        "<textarea rows=5 cols=70>" + result + "</textarea><br>");
+}
+
+function decode () {
+  const string = document.getElementById("textEncode").value;
+  const offset = parseInt(document.getElementById("offset").value);
+  const result = cipher.decode(offset, string);
+
+  
+  document.getElementById("result").innerHTML =
+        ("<h3 id='titulo2'>Mensagem decifrada:</h3>" +
+            "<textarea rows=5 cols=70>" + result + "</textarea><br>");
+}
+
+function limpar() {
+  location.reload();
+  
+}
 
